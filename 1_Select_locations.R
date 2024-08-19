c284ddebe90ed47cd1bad5076ffdf1df61cf01e6
Stewart <Laura.Stewart@ec.gc.ca>
2024-08-19 12:16:23 -0600

    i commit

--- 1_Select_locations.R
+++ 1_Select_locations.R
@@ -18,34 +18,44 @@ library(stars)
 library(terra)
 library(stringr)
 library(tidyverse)
+library(sfheaders)
 
 # bbox - bounding box
 # sta - ARU locations in spatial data format
 # lcc - land cover canada habitat raster
 
 
-# bounding box: -120 to -110 longitude, 58 to 61.5 latitude
-bbox <- st_polygon(list(cbind(c(-120,-110,-110,-120,-120), c(58,58,61.5,61.5,58))))
-crs(bbox)
-bbox = st_sfc(bbox,crs=4326) # WGS84
+lcc <- raster("../../can_land_cover_2020_30m_tif/CAN_NALCMS_landcover_2020_30m/data/CAN_NALCMS_landcover_2020_30m.tif")
+str(lcc)
+lcc.codes <- read.csv("../../can_land_cover_2020_30m_tif/lcc_codes.csv", header = T)
+st_crs(lcc) # WGS 84
+st_bbox(lcc)
 
+# bounding box: -120 to -110 longitude, 58 to 61.5 latitude
+
+latitude = c(58,58,61.5,61.5,58)
+longitude = c(-120,-110,-110,-120,-120)
+my.df = data.frame(latitude,longitude)
+
+bbox <- sfheaders::sf_polygon(
+  obj = my.df
+  , x = "longitude"
+  , y = "latitude")
+
+st_crs(bbox) <-  CRS("epsg:4326") # WGS84
+bbox = st_transform(bbox,st_crs(lcc))
+str(bbox)
+st_crs(bbox)
 
 sta=st_read("../../BOSS_ARU_Locations.csv", options=c("X_POSSIBLE_NAMES=longitude","Y_POSSIBLE_NAMES=latitude"))
 str(sta)
 sta<-sta[!is.na(sta$Latitude) & !is.na(sta$Longitude),]
-st_crs(sta) <- CRS("+init=epsg:4326") # WGS84
 sta = sta %>% filter(Latitude>58 & Latitude<61.5 & Longitude > -120 & Longitude < -110)
+st_crs(sta) <- CRS("epsg:4326") # WGS84
+sta = st_transform(sta,st_crs(lcc))
 
-
-lcc <- raster("../../can_land_cover_2020_30m_tif/CAN_NALCMS_landcover_2020_30m/data/CAN_NALCMS_landcover_2020_30m.tif")
-str(lcc)
-lcc.codes <- read.csv("../../can_land_cover_2020_30m_tif/lcc_codes.csv", header = T)
-st_crs(lcc) # WGS 84 
 # crop by same bounding box as above
 lcc <- raster::crop(lcc,st_bbox(bbox))
-
-
-
 
 # zones <- st_read("../6. Spatial data/Ecozones/Ecoregions")
 # st_crs(zones)  # NAD83
@@ -54,17 +64,17 @@ lcc <- raster::crop(lcc,st_bbox(bbox))
 
 #### Map it to make sure it looks ok ####
 
+fr <- sta%>% filter(ProjectID=="FR")
+lcc_tmp <- raster::crop(lcc,st_bbox(fr))
+
+tm_shape(fr) + # check that it looks they way I want it to
+  tm_dots() +
+  tm_shape(lcc_tmp) + tm_raster()
 
 
 
-#### Extract station-level ecoregion information ####
+#### Extract station-level land cover information ####
 
-sta <- st_transform(sta, st_crs(zones))
-stazones <- st_intersection(sta,zones)
-head(stazones)
-unique(stazones$REGION_NAM)
-abbr <- tibble(REGION_NAM = c("Peel River Plateau", "Mackenzie Mountains","Fort MacPherson Plain","Mackenzie River Plain"),
-               zabbr = c("PRP","MMO","FMP","MRP"))
-stazones = merge(stazones,abbr,by="REGION_NAM")
-stazones = stazones %>% dplyr::select(location,REGION_NAM,zabbr)
-
+sta_lcc <- raster::extract(lcc, sta) 
+summary(sta_lcc)
+sta$value <- sta_lcc
